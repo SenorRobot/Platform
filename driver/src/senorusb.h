56dfd4ec6f8c03dcc6d4ecb14f7212ff2123973a
Alex Crawford <adrofwarc@gmail.com>
2012-02-11 17:06:22 -0500

    LKM compiles; testing

--- driver/src/senorusb.h
+++ driver/src/senorusb.h
@@ -1,16 +1,19 @@
 #ifndef _SENORUSB_H_
 #define _SENORUSB_H_
 
+#include <linux/usb.h>
+
 #define USB_MINOR        1
 #define USB_INTERFACE_ID 1
-#define USB_ID_VENDOR    0x1234
-#define USB_ID_PRODUCT   0x5678
+#define USB_ID_VENDOR    0x03EB
+#define USB_ID_PRODUCT   0x0506
 
-struct usb_driver *driver_info = NULL;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alex Crawford");
 
-void *usb_probe(struct usb_device *dev, unsigned int interface, const struct usb_device_id *id_table);
-void usb_disconnect(struct usb_device *dev, void *context);
-int usb_ioctl(struct usb_device *dev, unsigned int code, void *buf);
+int usb_probe(struct usb_interface *intf, const struct usb_device_id *id_table);
+void usb_disconnect(struct usb_interface *intf);
+//int usb_ioctl(struct usb_device *dev, unsigned int code, void *buf);
 
 #endif
 
