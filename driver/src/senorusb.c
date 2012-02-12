c2789c251c2ee8f4056740852098a1d7c190c9e5
Alex Crawford <adrofwarc@gmail.com>
2012-02-12 15:41:52 -0500

    Driver loads and unloads

--- driver/src/senorusb.c
+++ driver/src/senorusb.c
@@ -1,20 +1,23 @@
 #include <linux/module.h>
 #include "senorusb.h"
 
+static struct usb_device_id id_table [] = {
+	{ USB_DEVICE(USB_ID_VENDOR, USB_ID_PRODUCT) },
+	{ },
+};
+MODULE_DEVICE_TABLE(usb, id_table);
+
 static struct usb_driver driver_info = {
 	.name = "SenorRobot",
-	.probe = &usb_probe
-	//.disconnect = &usb_disconnect,
+	.probe = &usb_probe,
+	.id_table = id_table,
+	.disconnect = &usb_disconnect
 	//.owner = THIS_MODULE,
 	//.driver_list = ??;
 	//.fops = ??;
-	//.minor = USB_MINOR,
+	//.minor = USB_MINOR
 	//.serialize = ??;
 	//.usb_ioctl;
-	//.id_table = static struct usb_device_id [] {
-	//	{ USB_DEVICE(USB_ID_VENDOR, USB_ID_PRODUCT); },
-	//	{ },
-	//}
 };
 
 int __init init_module() {
@@ -24,21 +27,28 @@ int __init init_module() {
 		return -1;
 	}
 
+	printk("REGISTERED DRIVER\n");
 	return 0;
 }
 
 void __exit cleanup_module() {
+	printk("DESTROYING DRIVER\n");
 	usb_deregister(&driver_info);
 }
 
 //void *usb_probe(struct usb_device *dev, unsigned int interface, const struct usb_device_id *id_table) {
 int usb_probe(struct usb_interface *intf, const struct usb_device_id *id_table) {
+	printk("usb_probe()\n");
+	printk(" idVendor:  %04X\n", interface_to_usbdev(intf)->descriptor.idVendor);
+	printk(" idProduct: %04X\n", interface_to_usbdev(intf)->descriptor.idProduct);
+	printk(" minor:     %04X\n", intf->minor);
 	if (interface_to_usbdev(intf)->descriptor.idVendor == USB_ID_VENDOR &&
-	    interface_to_usbdev(intf)->descriptor.idProduct == USB_ID_PRODUCT &&
-	    intf->minor == USB_INTERFACE_ID)
+	    interface_to_usbdev(intf)->descriptor.idProduct == USB_ID_PRODUCT)
 	{
 		try_module_get(THIS_MODULE);
-		printk("CREATED DRIVER\n");
+		printk("CONNECTED\n");
+
+		return 0;
 	}
 	printk("NOT RECOGNIZED\n");
 
